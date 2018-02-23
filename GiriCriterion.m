fb4800ea7d0d1567bd54961af8a7101a32fc63bb
0sektor0 <tomskiy_a@list.ru>
2018-02-23 14:43:23 +0300

    Giri criterion

--- GiriCriterion.m
+++ GiriCriterion.m
@@ -1,12 +1,34 @@
-function IsNormal = GiriCriterion(sample, table_giri)
+function IsNormal = GiriCriterion(sample, alpha)
+    %проверка на наличие известной апроксимации критических величин
+    if ~(alpha == 0.05)
+        error("invalid parametr alpha")
+    end
+    
+    %расчет параметров, необходимых для дальнейших вычмслений
     xavg = mean(sample);
     n = length(sample);
+    dsa = sample - xavg; %the Difference of the Sample and its Average. DSA 
+      
+    %расчет статистики
+    s =  sqrt(sum(dsa .* dsa)) / (n - 1);    
+    d = sum(abs(dsa)) / (n * s);
     
-    s = (sample - xavg);
-    s =  sqrt(sum(s .* s) / (n - 1));
+    %расчет мат ожид и дисперсии в соответсвие с их апроксимациями
+    exp = 0.797885 + 0.199471/n + 0.024934/n^2 - 0.031168/n^3 - 0.008182/n^4;
+    disp = 0.045070/n - 0.124648/n^2 + 0.084859/n^3 + 0.006323/n^4;
     
-    d = sum(sample - xavg) / (n * s);
+    %определене квантиля норм распределения заданного уровня
+    if alpha == 0.05
+        u = 1.645; %альфа квантиль стд нормального распределения уровня значимости 0.05 
+    end
     
-    %тут должно быть определение результата по таблице
-    IsNormal = true;
+    %определение нормальности распределения
+    d1 = exp + sqrt(disp)*(-u);
+    d2 = exp + sqrt(disp)*u;
+    
+    if d>d1 && d<d2
+        IsNormal = true;
+    else
+        IsNormal = false;
+    end
 end
